be52124da922d9878f5953f70bf29a1f4d77de7b
AndreM132 <andre.m132@hotmail.com>
2020-06-10 17:50:33 +0000

    updated

--- application/forms.py
+++ application/forms.py
@@ -4,18 +4,6 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from application.models import Users
 
 class PostForm(FlaskForm):
-    first_name = StringField('First Name',
-        validators = [
-            DataRequired(),
-            Length(min=2, max=30)
-        ]
-    )
-    last_name = StringField('Last Name',
-        validators = [
-            DataRequired(),
-            Length(min=2, max=30)
-        ]
-    )
     title = StringField('Title',
         validators = [
             DataRequired(),
@@ -32,6 +20,18 @@ class PostForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name',
+        validators = [
+            DataRequired(),
+            Length(min=2, max=30)
+        ]
+    )
+    last_name = StringField('Last Name',
+        validators = [
+            DataRequired(),
+            Length(min=2, max=30)
+        ]
+    )
     email = StringField('Email',
         validators = [
             DataRequired(),
@@ -50,7 +50,7 @@ class RegistrationForm(FlaskForm):
         ]
     )
     submit = SubmitField('Sign Up')
-
+    
     def validate_email(self, email):
         user = Users.query.filter_by(email=email.data).first()
         if user:
@@ -63,13 +63,12 @@ class LoginForm(FlaskForm):
             Email()
         ]
     )
-
     password = PasswordField('Password',
         validators=[
             DataRequired()
         ]
     )
-
+    
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
--- application/models.py
+++ application/models.py
@@ -1,6 +1,6 @@
-from application import db
-from application import login_manager
+from application import db, login_manager
 from flask_login import UserMixin
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(id):
@@ -8,11 +8,18 @@ def load_user(id):
 
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(500), nullable=False, unique=True)
     password = db.Column(db.String(500), nullable=False)
+    posts = db.relationship('Posts', backref='author', lazy=True)
+
+
 
     def __repr__(self):
-        return ''.join(['UserID: ', str(self.id), '\r\n', 'Email: ', self.email])
+        return ''.join(['UserID: ', str(self.id), '\r\n', 
+            'Name: ', self.first_name, ' ', self.last_name, '\r\n',
+            'Email: ', self.email])
 
 
 
@@ -20,13 +27,14 @@ class Users(db.Model, UserMixin):
 
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(30), nullable=False)
-    last_name = db.Column(db.String(30), nullable=False)
     title = db.Column(db.String(100), nullable=False, unique=True)
     content = db.Column(db.String(500), nullable=False, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
 
     def __repr__(self):
         return ''.join([
-            'User: ', self.first_name, ' ', self.last_name, '\r\n',
+            'User ID: ', str(self.id), '\r\n',
             'Title: ', self.title, '\r\n', self.content
             ])
